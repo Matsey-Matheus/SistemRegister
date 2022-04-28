0dd643d26b6164bd3607efe7ef521f6bd42ce51f
Matheus <mwrsgames1@gmail.com>
2022-04-28 04:07:46 -0300

    Version two.

--- SistemaRegistro/src/Registro/Programa.java
+++ SistemaRegistro/src/Registro/Programa.java
@@ -5,38 +5,26 @@ import java.util.Scanner;
 
 public class Programa {
 	
+	/*
+	 * VARIAVEIS GLOBAIS
+	 * USUARIO REGISTRO E LOGIN
+	 * SENHA REGISTRO E LO
+	 */
+	
+	// VAR USER
+	static String usuario;
+	static String usuarioLogin;
+	// VAR SENHA
+	static long senha;
+	static long senhaLogin;
+	
+	
 	public static void main(String[] args) {
 		
-		// VARIAVEIS
-		
-		// SCANNERS REGISTRO/LOGIN
-		
+		// SCANNERS
 		Scanner teclado = new Scanner(System.in);
 		Scanner teclado2;
 		
-		// VAR USER
-		String usuario;
-		String usuarioLogin;
-		
-		// VAR SENHA
-		long senha;
-		long senhaLogin;
-		
-		// SCANNERS PERSONALIZAÇÃO
-		
-		Scanner perso = new Scanner(System.in);
-		Scanner perso2 = new Scanner(System.in);
-		Scanner perso3 = new Scanner(System.in);
-		Scanner perso4 = new Scanner(System.in);
-		
-		// VAR PERSONALIZAÇÃO DE PERFIL
-		String nome;
-		String nick;
-		int idade;
-		String comida;
-		String jogo;
-		String hobby;
-		long vadias;
 		
 		
 		System.out.println("------------");
@@ -104,63 +92,90 @@ public class Programa {
 		} while (!usuarioLogin.equals(usuario) || senhaLogin != senha);
 			
 		
-			// CONTINUAÇÃO CODIGO
+			Programa entrada = new Programa();
 			
-			System.out.println("");
-			System.out.println("Personalize seu perfil");
-			System.out.println("----------------------");
-			
-			System.out.print("Nome: ");
-			nome = perso.nextLine();
-			
-			System.out.print("Idade: ");
-			idade = perso.nextInt();
-			
-			System.out.print("Nick: ");
-			nick = perso2.nextLine();
-			
-			System.out.print("Jogo favorito: ");
-			jogo = perso2.nextLine();
-			
-			System.out.print("Comida favorita: ");
-			comida = perso3.nextLine();
-			
-			// MEMERS KKK
-			
-			System.out.print("Hobby: ");
-			hobby = perso3.nextLine();
-			
-			if (hobby.equalsIgnoreCase("sentar")) {
-				
-				System.out.println("");
-				System.out.println("Ta de parabéns.");
-				System.out.println("");
-				
-			}
-			
-			System.out.print("Quantas vadias tu tem? ");
-			vadias = perso4.nextLong();
-			
-			// MENSAGENS
-			
-			System.out.println("");
-			System.out.println("Perfil");
-			System.out.println("------");
-			System.out.println(nome + ", " + idade + " anos.");
-			System.out.println("Jogo " + jogo + " (" + nick + " meu nick.).");
-			System.out.println("Minha comida favorita eh " + comida + ".");
-			System.out.println("Meu hobby eh " + hobby + ".");
-			System.out.println("Atualmente tenho " + vadias + " vadias.");
+			entrada.Personalizacao();
 			
 			teclado.close();
 			teclado2.close();
-			perso.close();
-			perso2.close();
-			perso3.close();
-			perso4.close();
+			
+	}
+	
+	
+	public void Personalizacao() {
 		
+		// SCANNERS PERSONALIZAÇÃO
+		
+		Scanner perso = new Scanner(System.in);
+		Scanner perso2 = new Scanner(System.in);
+		Scanner perso3 = new Scanner(System.in);
+		
+		// VAR PERSONALIZAÇÃO DE PERFIL
+		String nome;
+		String nick;
+		int idade;
+		String comida;
+		String jogo;
+		String hobby;
+		
+		// CODIGO
+		
+		System.out.println("");
+		System.out.println("Personalize seu perfil");
+		System.out.println("----------------------");
+		
+		System.out.print("Nome: ");
+		nome = perso.nextLine();
+		
+		System.out.print("Idade: ");
+		idade = perso.nextInt();
+		
+		System.out.print("Nick: ");
+		nick = perso2.nextLine();
+		
+		System.out.print("Jogo favorito: ");
+		jogo = perso2.nextLine();
+		
+		System.out.print("Comida favorita: ");
+		comida = perso3.nextLine();
+		
+		// MEMERS KKK
+		
+		System.out.print("Hobby: ");
+		hobby = perso3.nextLine();
+		
+		if (hobby.equalsIgnoreCase("sentar")) {
 			
+			System.out.println("");
+			System.out.println("Ta de parabéns.");
+			System.out.println("");
 			
+		}
+		
+		// MENSAGENS
+		
+		System.out.println("");
+		System.out.println("Perfil");
+		System.out.println("------");
+		System.out.println(nome + ", " + idade + " anos.");
+		System.out.println("Jogo " + jogo + " (" + nick + " meu nick.).");
+		System.out.println("Minha comida favorita eh " + comida + ".");
+		System.out.println("Meu hobby eh " + hobby + ".");
+		
+		perso.close();
+		perso2.close();
+		perso3.close();
+		
+		Programa entrada = new Programa();
+		
+		entrada.Saida();
+		
+	}
+	
+	
+	public void Saida() {
+		
+		
 	}
 	
 }
